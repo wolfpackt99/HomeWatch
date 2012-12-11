0eb0c484aa68cfadab864f1420d94f39b89769d9
Trent Jones <trenton.jones@gmail.com>
2012-12-11 00:51:51 -0500

    ConsoleApp Added

---
+++ TDJ.HomeWatch.ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDJ.HomeWatch.Business;
+
+namespace TDJ.HomeWatch.ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var connection = new HubConnection(Config.SignalRUri);
+            connection.ConnectionId = Config.ConsoleID;
+            var hub = connection.CreateHubProxy("InfoHub");
+
+            connection.Start().ContinueWith(task => 
+            {
+                if (!task.IsFaulted)
+                {
+                    Console.WriteLine("Connected...");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to start: {0}", task.Exception.GetBaseException());
+                }
+            }).Wait();
+
+            hub.Invoke<string>("Send", "From the console");
+
+            hub.On("fromServer", data => { 
+                Console.WriteLine(data);
+            });
+
+            hub.On("addMessage", data =>
+            {
+                Console.WriteLine(data);
+            });
+
+            // Keep going until somebody hits 'x'
+            while (true)
+            {
+                ConsoleKeyInfo ki = Console.ReadKey(true);
+                if (ki.Key == ConsoleKey.X)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
